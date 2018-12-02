9098ffea994c8d334e370e80c45b329a8d1b0e18
sangumee <super2451894@gmail.com>
2018-12-02 21:48:43 +0900

    String
    Syntax String

---
+++ syntax/hello.py
@@ -0,0 +1,5 @@
+#!C:\Users\super\AppData\Local\Programs\Python\Python37-32\python.exe
+a = 3+4+5
+b = a/3
+print("content-type: text/html; charset=utf-8\n")
+print(b)
---
+++ syntax/string.py
@@ -0,0 +1,20 @@
+print('Hello World')
+print("Hello World")
+print("Hello'o' World")
+#escape
+print("Hello'o' \"w\"orld")
+#newLine
+print('H')
+print('E')
+print('L')
+print('L')
+print('O')
+print('H\ne\nl\nl\no')
+#docstring
+print('''
+H
+E
+L
+L
+O
+''')
