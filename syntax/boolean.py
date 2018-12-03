6b8cae094ef5d5883062cbaa02b5f0a1e8ddb55f
sangumee <super2451894@gmail.com>
2018-12-03 13:31:54 +0900

    Create boolean.py
    - boolean.py

---
+++ syntax/boolean.py
@@ -0,0 +1,26 @@
+import os.path
+
+# Number
+print(1)  # Integer
+
+# String
+print('Hello Python')
+
+# Boolean
+print(True)
+print(False)
+
+# Expression
+print(1+1)
+print('Hello '+'Python')  # Hello Python
+
+# Comparision Operator
+print(1 == 1)  # True
+print(1 == 2)  # False
+print(1 > 2)  # False
+
+# Membership Operator
+print('world' in 'Hello world')  # check string exists
+
+# check file exists
+print(os.path.exists('boolean.py'))
