38ecd307d854023a612f72448822661ac1c5da46
sangumee <super2451894@gmail.com>
2018-12-03 13:42:17 +0900

    Create conditional.py
    - Create Login App with conditional syntax

---
+++ syntax/conditional.py
@@ -0,0 +1,19 @@
+#!C:\Users\super\AppData\Local\Programs\Python\Python37-32\python.exe
+
+user_id = input('Enter ID : ')
+user_pwd = input('Enter Password : ')
+
+if user_id == 'sangumee':
+    if user_pwd == '111111':
+        print('Login Success')
+    else:
+        print('Wrong Password!!')
+else:
+    print('Wrong ID')
+
+'''
+if user_input == '111111':
+    print('Login Success')
+else:
+    print('Wrong Password!!')
+'''
