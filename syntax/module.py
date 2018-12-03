fab1f68c5f32b725387fea7fa783c6357d69cd3f
sangumee <super2451894@gmail.com>
2018-12-03 17:00:24 +0900

    syntax Module
    - Module Syntax

---
+++ syntax/module.py
@@ -0,0 +1,12 @@
+'''
+import math
+print(math.average(1,2,3))
+print(math.plus(1,2))
+print(math.pi)
+'''
+
+from mathfunc import average, plus, pi
+
+print(average(1, 2, 3))
+print(plus(1, 2))
+print(pi)
