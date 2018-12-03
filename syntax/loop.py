c7cb112dc15decd6b2ad2e6cade7cecd0ff27421
sangumee <super2451894@gmail.com>
2018-12-03 14:52:41 +0900

    Create loop.py
    - Loop Syntax

---
+++ syntax/loop.py
@@ -0,0 +1,9 @@
+# List Loop
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+for value in alphabet:
+    print(value)
+print('------range-------')
+
+# Range Loop
+for value in range(10):
+    print(value)
