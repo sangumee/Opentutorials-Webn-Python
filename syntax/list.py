376c127b4d54c61244c76f7e1d27ae34b0a06df2
sangumee <super2451894@gmail.com>
2018-12-03 14:41:40 +0900

    Create list.py
    - list styntax

---
+++ syntax/list.py
@@ -0,0 +1,17 @@
+list = [1, 'four', 9, 15, '33', 'apple']
+print(list)
+
+# List Output
+print(len(list))
+
+# Modify List
+list[1] = 4
+print(list)
+
+# Delete element in list
+del list[2]
+print(list)
+
+# add element to list
+list.append('sangumee')
+print(list)
