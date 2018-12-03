6ad2bd08035c22068612fa2e6adb167cb0da0a53
sangumee <super2451894@gmail.com>
2018-12-03 13:49:01 +0900

    Update conditional.py
    - complex conditional syntax

--- syntax/conditional.py
+++ syntax/conditional.py
@@ -3,6 +3,15 @@
 user_id = input('Enter ID : ')
 user_pwd = input('Enter Password : ')
 
+if user_id == 'sangumee' and user_pwd == '111111':
+    print('Login Success with ' + user_id)
+elif user_id == 'egoing' and user_pwd == '222222':
+    print('Login Success with '+user_id)
+else:
+    print('Wrong Information')
+
+
+'''
 if user_id == 'sangumee':
     if user_pwd == '111111':
         print('Login Success')
@@ -10,6 +19,7 @@ if user_id == 'sangumee':
         print('Wrong Password!!')
 else:
     print('Wrong ID')
+'''
 
 '''
 if user_input == '111111':
